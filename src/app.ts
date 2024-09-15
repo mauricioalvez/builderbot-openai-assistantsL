98ad35bf857cab561684d51d547194c1160a60e7
mauricioalvez <licmauricioalvez@gmail.com>
2024-09-15 13:37:11 -0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -72,7 +72,7 @@ const voiceNoteFlow = addKeyword<BaileysProvider, MemoryDB>(EVENTS.VOICE_NOTE)
         await recording(ctx, provider)
         try {
             // Guardar archivo de audio localmente
-            const localPath = await provider.saveFile(ctx);
+            const localPath = await provider.saveFile(ctx { path: 'audios'});
             console.log('Ruta del archivo de audio local:', localPath);
 
             // Leer el archivo de audio
