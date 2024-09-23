5fd957c417a4ea23834e89bf00207778591768d4
mauricioalvez <licmauricioalvez@gmail.com>
2024-09-22 21:37:51 -0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -186,7 +186,7 @@ const main = async () => {
      * Flujo del bot
      * @type {import('@builderbot/bot').Flow<BaileysProvider, MemoryDB>}
      */
-    const adapterFlow = createFlow([welcomeFlow, voiceNoteFlow, mediaFlow, documentFlow]);
+    const adapterFlow = createFlow([welcomeFlow, voiceNoteFlow, mediaFlow, documentFlow, denunciaFlow]);
 
     /**
      * Proveedor de servicios de mensajería
