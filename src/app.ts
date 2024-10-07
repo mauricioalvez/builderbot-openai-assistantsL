0c120cf3b3183dd471adbd1ac1ab8adf11def4e1
mauricioalvez <licmauricioalvez@gmail.com>
2024-10-07 19:48:05 -0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -104,13 +104,12 @@ const voiceNoteFlow = addKeyword<BaileysProvider, MemoryDB>(EVENTS.VOICE_NOTE)
             });
             const transcription = transcribeResponse.text;
             console.log('Transcripción del audio:', transcription);
-            await flowDynamic('Transcripción del audio:', transcription);
 
             // Obtener respuesta del asistente de OpenAI basado en la transcripción
             
             const askToAiResponse = await toAsk(ASSISTANT_ID, transcription, state);
             console.log('Respuesta del asistente de OpenAI:', askToAiResponse);
-            //await flowDynamic('Transcripción del audio: ' + transcription);
+            await flowDynamic('Transcripción del audio: ' + transcription);
             await flowDynamic(askToAiResponse);
 
 
