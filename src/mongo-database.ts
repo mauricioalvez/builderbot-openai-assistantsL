6fbadedb0ead5108cf0c35e9e94f25866e13a2d8
mauricioalvez <licmauricioalvez@gmail.com>
2024-10-22 01:04:46 -0300

    Update mongo-database.ts

--- src/mongo-database.ts
+++ src/mongo-database.ts
@@ -3,5 +3,5 @@ import { MongoDB } from '@builderbot/database-mongo'
 export type IDatabase = typeof MongoDB
 export const adapterDB = new MongoDB({
     dbUri: process.env.MONGO_DB_URI ?? '',
-    dbName: 'Cluster0',
+    dbName: 'lupita',
 })
