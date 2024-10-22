13a9be5c97a5fce3509ec7596d4b4ab4a594a5ef
mauricioalvez <licmauricioalvez@gmail.com>
2024-10-22 01:10:50 -0300

    Delete src/mongo-database.ts

--- src/mongo-database.ts
+++
@@ -1,7 +0,0 @@
-import { MongoDB } from '@builderbot/database-mongo'
-
-export type IDatabase = typeof MongoDB
-export const adapterDB = new MongoDB({
-    dbUri: process.env.MONGO_DB_URI ?? '',
-    dbName: 'lupita',
-})
