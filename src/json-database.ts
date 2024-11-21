b7271ca4357e04ffd918f663a2226f1ff96ddf78
mauricioalvez <licmauricioalvez@gmail.com>
2024-11-21 00:06:08 -0300

    Update json-database.ts

--- src/json-database.ts
+++ src/json-database.ts
@@ -1,4 +1,4 @@
 import { JsonFileDB } from '@builderbot/database-json';
 
 export type IDatabase = typeof JsonFileDB
-export const adapterDB = new JsonFileDB('./db.json');
+export const adapterDB = new JsonFileDB({ filename: 'db1.json' });
